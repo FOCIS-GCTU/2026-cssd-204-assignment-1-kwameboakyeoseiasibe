fa3fa7971042f8bb1ae779a67f9991e5e97f58af
kwameboakyeoseiasibe <2425401614@live.gctu.edu.gh>
2026-05-30 16:12:33 -0700

    Add files via upload

---
+++ intials.py
@@ -0,0 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Osei-Asibe Kwame Boakye
+# STUDENT ID: 2425401614
+# Email: 2425401614@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Osei-Asibe Kwame Boakye, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+def main():
+    #prints my initials in stylized large letters
+    print()
+    print("...OAK")
+    print()
+
+    print("...OOOOOOOOO............AAAAAA.............KK........KK......BBBBBBBB...........")
+    print("...OO.....OO...........AA.....AA...........KK......KK........BB......BB.........")
+    print("...OO.....OO..........AA........AA.........KK....KK..........BB.......BB........")
+    print("...OO.....OO.........AA..........AA........KK..KK............BBB....BB..........")
+    print("...OO.....OO.........AAAAAAAAAAAAAA........KKKK..............BBBBBBBBB..........")
+    print("...OO.....OO.........AA..........AA........KK..KK............BB.......BB........")
+    print("...OO.....OO.........AA..........AA........KK....KK..........BB........BB........")
+    print("...OO.....OO.........AA..........AA........KK.....KK.........BB........BB.......")
+    print("...OO.....OO...**....AA..........AA...**...KK......KK...**...BB........BB.**....")
+    print("...OOOOOOOOO...**....AA..........AA...**...KK.......KK...**..BBBBBBBBB....**....")
+
+main()
